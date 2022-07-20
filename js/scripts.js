efdef54afc05d1798b46a581b08293d4e3df92f3
jrfoliva <jrfoliva@gmail.com>
2022-07-20 12:57:55 -0300

    Versão PROD 1.20

--- js/scripts.js
+++ js/scripts.js
@@ -14,9 +14,100 @@
 
 
 // Definição dos textos que irão nas DIVs 
+const areaPenal = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO PENAL",
+   content:
+   `
+   <p>
+      O direito penal é o ramo do direito público que versa sobre o regramento do poder de punir do Estado, instituindo as infrações que da matéria fazem parte, bem como as sanções aplicáveis.
+   </p>
+   <p>
+      Considerado por muitos como o ramo mais rígido do direito, sendo a última alternativa para repressão e prevenção de práticas que se enquadram às normas caracterizadas como crimes.
+   </p>
+   <p>
+      Nossas áreas de atuação que envolvem o direito penal são:
+      <ul>
+         <li>Assistência de acusação;</li>
+         <li>Representação nas ações penais públicas;</li>
+         <li>Ação penal privada (calúnia, injúria e difamação);</li>
+         <li>Acompanhamento na execução penal.</li>
+      </ul>
+   </p>
+   `
+};
+
+const areaImobiliario = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO IMOBILIÁRIO",
+   content:
+   `
+   <p>
+      O direito imobiliário é um ramo do direito privado que lida, como o próprio nome já aponta, com as relações jurídicas que envolvam a posse, compra, venda, doação, sucessão, troca e propriedade de bens imóveis, inclusive incorporação imobiliária.
+   </p>
+   <p>
+      Embora tenha o objeto de estudo bem definido, trabalha com diferentes campos do direito, como o direito contratual, do consumidor, da família, civil, dentre outros.
+   </p>
+   <p>
+      Portanto, trata-se de uma área que sempre apresentará relevância social, visto que há uma grande circulação de imóveis entre diferentes pessoas na sociedade em que vivemos hoje.
+   </p>
+   <p>
+      Ademais, como o grande mercado imobiliário brasileiro vem se modificando e se sofisticando nos últimos anos, a equipe VOPTA pensando nisso, conta com uma equipe dedicada e organizada nas seguintes áreas:
+      <ul>
+         <li>Negócios imobiliários (compra e venda de imóveis, incorporações, imobiliárias e locação);</li>
+         <li>Direito imobiliário para condomínios;</li>
+         <li>Extinção de condomínio;</li>
+         <li>Distrato de Compra e Venda; </li>
+         <li>Leilão de Imóveis (arrematação).</li>
+      </ul>
+   </p>
+   `
+};
+
+
+
+const areaEmpresarial = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO EMPRESARIAL",
+   content:
+   `
+   <p>
+      O Direito Empresarial, também chamado de Direito Comercial, é uma área do direito privado e que tem por interesse as relações entre empresários e os atos relativos à própria empresa. Atribuindo uma forma mais abrangente ao que diz respeito à sua atuação, busca regular as relações das estruturas geradoras de riquezas e particulares, concessão de crédito, sinais distintivos, relações entre sócios, e, na pior das hipóteses, a falência.
+   </p>
+   <p>
+      Tal disciplina tem por objeto as estruturas de interesse dos atuantes das atividades econômicas de produção ou circulação de bens ou prestação de serviços que, em geral, a sociedade necessita para se manter firme. Envolve não apenas a intermediação de mercadorias, mas inclui também as relações bancárias, industriais, securitárias, prestadoras de serviços e, em suma, praticamente toda estrutura organizada que visa o lucro.
+   </p>
+   <p>
+      Nossas áreas de atuação envolvem:
+      <ul>
+         <li>Sociedades Empresárias;</li>
+         <li>Estabelecimento Empresarial;</li>
+         <li>Estabelecimento Empresarial;</li>
+         <li>Publicidade;</li>
+         <li>Títulos de Crédito;</li>
+         <li>Comércio Eletrônico;</li>
+         <li>Contratos;</li>
+         <li>Empresário e Relação de Consumo;</li>
+         <li>Falência e Recuperação de Empresas.</li>
+      </ul>
+   </p>
+
+   ` 
+};
 const areaSaude = {
     advogado: "Matheus Bueno",
     srcPhoto: "../image/Matheus.png",
+    phone: "(11) 96308-3046",
+    email: "contato@vopta.com.br",
     title: "DIREITO MÉDICO E DA SAÚDE",
     content: 
       `
@@ -56,9 +147,162 @@ const areaSaude = {
       <p>
          A equipe VOPTA, estuda o mercado de inovações e implementações de recursos preventivos jurídicos para seus clientes, buscando uma atuação conjunta de todos os ramos jurídicos que uma Clínica ou Hospital precisa, integrando o direito médico ao empresarial, tributário, seguro e resseguros, digital e outros que ajudam a elevar o sucesso de seus clientes. 
       </p>
-      `,
-    phone: "(11) 95233-0605",
-    email: "mtofano@vopta.com.br"
+      `
+};
+
+const areaFamilia = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO DE FAMÍLIA",
+   content:
+   `
+   <p>
+      Embora não tenhamos uma conceituação concreta sobre o que é família, a Constituição Federal juntamente com o Código Civil nos fornecem parâmetros que podemos utilizar para entender melhor do que se trata tal caracterização.
+   </p>
+   <p>
+      Podemos dizer que a família é o conjunto de pessoas conectadas por um vínculo consanguíneo, procedendo de um tronco ancestral comum, juntamente com aquelas incluídas pela afinidade ou adoção.
+   </p>
+   <p>
+      O Direito de Família busca estabelecer normas (regras e princípios) que visam nortear a relação familiar e fornecer proteção do bem que, segundo a Carta Magna (art. 226, CF) formam a base da sociedade.
+   </p>
+   <p>
+      Vale mencionar que, por mais que não se confunda com direitos patrimoniais ou obrigacionais propriamente ditos, o Direito de Família se vale de tais institutos para estabelecer a devida proteção familiar de que tal relação jurídica necessita.
+   </p>
+   <p>
+      É importante ressaltar que o conceito de família, pela tendência da doutrina, bem como a evolução social constante na qual passamos, nos esclarece que podemos ampliar a ideia para formas familiares não previstas no nosso ordenamento jurídico. 
+   </p>
+   <p>
+      Sendo assim, há que se falar em diversas formas familiares, além da tradicional como a família matrimonial (advinda do casamento), mas também a família informal (advinda da união estável), família monoparental (advinda da formação de um dos genitores e seus filhos), família anaparental (constituída somente pelos filhos), família homoafetiva (formada por pessoas do mesmo sexo), família eudemonista (formada pelo vínculo afetivo).
+   </p>
+   <p>
+      A nossa atuação consultiva e /ou contenciosa envolve:
+      <ul>
+         <li>Alimentos (pedido de concessão, revisão, exoneração e execução);</li>
+         <li>Casamento (regime de bens e pacto antenupcial);</li>
+         <li>Divórcio;</li>
+         <li>Doação;</li>
+         <li>Guarda e regulamentação de visita de menores;</li>
+      </ul>
+   </p>
+   `
+};
+
+const areaDigital = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO DIGITAL E PROTEÇÃO DE USO DE DADOS",
+   content:
+   `
+   <p>
+      O direito digital trata de um ramo bastante novo, uma vez que lida diretamente com o uso da tecnologia, em particular da internet e dos meios digitais, compreendendo uma gama de assuntos complexos e extensos, desde a utilização da tecnologia da informação até a divulgação de dados.
+   </p>
+   <p>
+      Uma vez que a tecnologia e o uso da internet são cada vez mais interconectados com todas as relações humanas, o direito digital torna-se mais relevante para a proteção das informações pessoais.
+   </p>
+   <p>
+      Esse ramo, portanto, se dedica ao estudo das regulamentações, desde leis, resoluções de conselhos até jurisprudências atuais que envolvem as atividades. 
+   </p>
+   <p>
+      Com a era digital e com a informatização das coisas, surge no meio desse desenvolvimento um problema natural: onde há tecnologia, há também mais riscos de ataques virtuais, roubos, vazamento e destruição de dados e informações relevantes para pessoas, empresas e governos.
+   </p>
+   <p>
+      Pensando assim, a equipe VOPTA atua nas áreas consultiva e/ou contenciosa, abrangendo:
+      <ul>
+         <li>Processo criminais;</li>
+         <li>Contratos;</li>
+         <li>Responsabilidade Civil na internet;</li>
+         <li>Gestão de risco;</li>
+         <li>Compliance digital;</li>
+         <li>Legal Tech e Start Up;</li>
+         <li>Propriedade intelectual e material;</li>
+         <li>Comércio eletrônico;</li>
+         <li>Implementação de proteção e gestão de dados LGPD.</li>
+      </ul>
+   </p>
+   ` 
+};
+
+const areaConsumidor = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO DO CONSUMIDOR",
+   content:
+   `
+   <p>
+      O Direito do Consumidor, embora intimamente ligado ao Direito Civil, visa a devida aplicação nos casos em que, tendo em vista o desdobramento de uma relação jurídica, o fato incorre em uma relação jurídica de consumo.
+   </p>
+   <p>
+      Para se ter presente uma relação jurídica de consumo é necessário que nessa relação estejam presentes, consumidor e fornecedor (elementos subjetivos), e a aquisição de um produto ou prestação de serviço (elementos objetivos).
+   </p>
+   <p>
+      Diz-se fornecedor toda pessoa física ou jurídica, pública ou privada, nacional ou estrangeira, ainda que caracterizem entes despersonalizados, que desenvolvam atividade de produção, montagem, criação, construção, transformação, importação, exportação, distribuição ou comercialização de produtos ou prestação de serviços.
+   </p>
+   <p>
+      O consumidor é toda pessoa física ou jurídica, que adquire um produto ou serviço como destinatário final, isto é, que não utilize o produto adquirido ou o serviço contratado para fins comerciais.
+   </p>
+   <p>
+      Equipara-se a consumidor a coletividade de pessoas, mesmo na impossibilidade de mensurar sua quantidade, que haja intervindo na relação de consumo, ou ainda aquelas que sejam vítimas de algum evento.
+   </p>
+   <p>
+      O Direito do Consumidor busca a proteção do consumidor, tendo em vista este ser a parte vulnerável da relação jurídica de consumo, estabelecendo, portanto, uma relação mais equitativa para o deslinde de causas.
+   </p>
+   <p>
+      Nossas áreas de atuação consultiva e /ou contenciosa envolve:
+      <ul>
+         <li>Responsabilidade pelo defeito do produto ou serviço;</li>
+         <li>Responsabilidade pelo vício do produto ou serviço;</li>
+         <li>Proteção a saúde e segurança na relação de consumo;</li>
+         <li>Boas práticas comerciais envolvendo a oferta e publicidade;</li>
+         <li>Consultoria às práticas abusivas na relação de consumo;</li>
+         <li>Regras para a cobrança de dívidas;</li>
+         <li>Proteção contratual na relação de consumo;</li>
+         <li>Revisão de seguros, resseguros e previdência privada.</li>
+      </ul>
+   </p>
+   `
+};
+
+const areaTrabalho = {
+   advogado: "Matheus Bueno",
+   srcPhoto: "../image/Matheus.png",
+   phone: "(11) 96308-3046",
+   email: "contato@vopta.com.br",
+   title: "DIREITO DO TRABALHO",
+   content:
+   `
+   <p>
+      Direito do Trabalho (ou Direito Trabalhista) é o ramo do direito focado em regular as relações de trabalho, envolvendo um trabalhador (ou empregado) e um tomador de seus serviços (ou empregador).
+   </p>
+   <p>
+      O trabalho, objeto de estudo por este ramo do direito, pode ser definido como todo e qualquer dispêndio de energia humana, física ou intelectual, com intuito de produzir bens ou serviços.
+   </p>
+   <p>
+      Como todo instituto jurídico, é composto por regras e princípios, e, neste caso, aplicáveis na relação jurídica de trabalho ou até mesmo em situações equiparáveis, visando proteger a condição social do trabalhador.
+   </p>
+   <p>
+      É tido como um dos ramos do direito privado mais importantes, visando um elemento indispensável para a sociedade, o trabalho, visando o bem estar e evolução do cuidado com o empregado e sua relação com a empresa. 
+   </p>
+   <p>
+      Nossas áreas de atuação envolvem:
+      <ul>
+         <li>Defesa em reclamações trabalhistas individuais e coletivas;</li>
+         <li>Reestruturação e redução de empregados em operações de fusões e aquisições, extinção de estabelecimentos ou encerramento de atividades;</li>
+         <li>Terceirização de serviços e contratação de fornecedores e representantes independentes;</li>
+         <li>Revisão legal de estruturas de salários e benefícios;</li>
+         <li>Planos de incentivo (bônus, opções de ações, unidades de ações restritas e planos de participação nos lucros);</li>
+         <li>Contratação e desligamento de empregados e executivos;</li>
+         <li>Revisão de políticas internas da empresa;</li>
+         <li>Negociação coletiva com sindicatos de diversos setores;</li>
+         <li>Questões relativas às atividades com tecnologia.</li>
+      </ul>
+   </p>
+   `
 };
 
 //Monitora eventos click nos ítens de atuaçao, passando à função a classe clicada.
@@ -97,6 +341,8 @@ function preparaItemChamado(atuacao){
             apresentarDiv(areaConsumidor);
             break;
          case 'trabalho':
+            apresentarDiv(areaTrabalho);
+            break;
       }   
 }
 
@@ -111,7 +357,7 @@ function apresentarDiv(area){
          </div>
          <div class="footer">
             <p style="text-align:center;">Contato: `+area.phone+`<br>
-               Email: `+area.email+`</p>
+               E-mail: `+area.email+`</p>
             <button type="button" id="btFechar" class="btn btn-secondary" data-bs-dismiss="modal" onclick="removeElem()">Fechar</button>
          </div>
       </div>
